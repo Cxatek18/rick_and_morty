ea6c6e0a97abe643e96c1c537bc66714ba719f42
Cxatek18 <griga2888@mail.ru>
2025-04-15 11:18:47 +0500

    initial RM-1-added-splash-screen

--- app/src/main/java/com/example/rick_and_morty/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/rick_and_morty/presentation/main/MainActivity.kt
@@ -8,18 +8,16 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.rick_and_morty.core.ui.theme.Rick_and_mortyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        installSplashScreen()
         setContent {
-            Rick_and_mortyTheme {
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//
-//                }
-            }
+            Rick_and_mortyTheme {}
         }
     }
 }
