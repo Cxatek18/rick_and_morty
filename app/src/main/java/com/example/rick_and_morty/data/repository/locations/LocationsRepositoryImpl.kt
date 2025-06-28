c123cf6045295f66ba330e6dce3fce9dfc399992
Cxatek18 <griga2888@mail.ru>
2025-06-28 14:24:24 +0500

    RM-19: Added pagination to the location page

--- app/src/main/java/com/example/rick_and_morty/data/repository/locations/LocationsRepositoryImpl.kt
+++ app/src/main/java/com/example/rick_and_morty/data/repository/locations/LocationsRepositoryImpl.kt
@@ -1,13 +1,12 @@
 package com.example.rick_and_morty.data.repository.locations
 
-import com.example.rick_and_morty.data.remote.error_handler.mapOnSuccess
-import com.example.rick_and_morty.data.remote.error_handler.safeApiCall
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import com.example.rick_and_morty.data.remote.network.ApiService
-import com.example.rick_and_morty.domain.module.error_handler.ApiResult
-import com.example.rick_and_morty.domain.module.locations.LocationsResultModel
+import com.example.rick_and_morty.domain.module.locations.LocationItemModel
 import com.example.rick_and_morty.domain.repository.locations.ILocationsRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class LocationsRepositoryImpl @Inject constructor(
@@ -18,21 +17,21 @@ class LocationsRepositoryImpl @Inject constructor(
         nameLocation: String?,
         typeLocation: String?,
         dimensionLocation: String?
-    ): Flow<ApiResult<LocationsResultModel>> = flow {
-        emit(
-            safeApiCall {
-                apiService.getListLocations(
-                    name = nameLocation,
-                    type = typeLocation,
-                    dimension = dimensionLocation
+    ): Flow<PagingData<LocationItemModel>> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = 20,
+                prefetchDistance = 5,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = {
+                LocationDataSource(
+                    apiService,
+                    nameLocation,
+                    typeLocation,
+                    dimensionLocation
                 )
             }
-                .mapOnSuccess { response ->
-                    LocationsResultModel(
-                        info = response.info,
-                        results = response.results
-                    )
-                }
-        )
+        ).flow
     }
 }
--- app/src/main/java/com/example/rick_and_morty/presentation/screens/locations_list/LocationsListManagementSuccess.kt
+++ app/src/main/java/com/example/rick_and_morty/presentation/screens/locations_list/LocationsListManagementSuccess.kt
@@ -2,32 +2,39 @@ package com.example.rick_and_morty.presentation.screens.locations_list
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.paging.PagingData
+import androidx.paging.compose.collectAsLazyPagingItems
 import com.example.rick_and_morty.core.ui.components.locations.LocationsCard
 import com.example.rick_and_morty.domain.module.locations.LocationItemModel
+import kotlinx.coroutines.flow.Flow
 
 @Composable
 fun LocationsListManagementSuccess(
     modifier: Modifier = Modifier,
-    locations: List<LocationItemModel>,
+    locations: Flow<PagingData<LocationItemModel>>,
     onClickNavigateToLocationsDetail: (episodeId: Int) -> Unit
 ) {
+    val lazyPagingItems = locations.collectAsLazyPagingItems()
+
     LazyColumn(
         modifier = modifier.fillMaxWidth()
     ) {
-        items(locations, key = { it.id }) { location ->
-            LocationsCard(
-                modifier = Modifier,
-                locationId = location.id,
-                locationName = location.name,
-                locationType = location.type,
-                locationDimension = location.dimension,
-                onClickNavigateToLocationsDetail = {
-                    onClickNavigateToLocationsDetail(it)
-                }
-            )
+        items(lazyPagingItems.itemCount, key = { it }) { number ->
+            val location = lazyPagingItems[number]
+            if (location != null) {
+                LocationsCard(
+                    modifier = Modifier,
+                    locationId = location.id,
+                    locationName = location.name,
+                    locationType = location.type,
+                    locationDimension = location.dimension,
+                    onClickNavigateToLocationsDetail = {
+                        onClickNavigateToLocationsDetail(it)
+                    }
+                )
+            }
         }
     }
 }
